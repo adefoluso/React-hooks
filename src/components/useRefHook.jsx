7c65076ec2260ddf3898ec3b8d01baab6c626dd8
adefoluso <adedejifoluso@gmail.com>
2022-08-03 23:00:00 +0100

    useRef hook

--- src/components/useRefHook.jsx
+++ src/components/useRefHook.jsx
@@ -1,12 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useRef,  useEffect } from "react";
+
+//A ref is similar to state in that it persits between render of components, ref does not cause a component to reupdate after it renders or gets changed
+
+//ref is similar to state and u can store a previous value in it
+
 
 export function UseRefHook() {
-  const [name, setname] = useState("");
+    const [name, setName] = useState('');
+    const prevName = useRef('');
+ 
+//    function focus() {
+//       inputRef.current.focus()
+//    }
+
+   useEffect(() => {
+    prevName.current = name 
+   }, [name])
 
   return (
     <>
-      <input value={name} onChange={(e) => setname(e.target.value)} />
-      <div>My name is {name}</div>
+      <input value={name} onChange={e => setName(e.target.value)} />
+      <div>My name is {name} and it used to be {prevName.current}</div>
+     {/* <button onClick={focus}>Focus</button> */}
     </>
   );
 }
